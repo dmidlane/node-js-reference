75fcb2f3a2a8cbd52c703a49abc94e0288a43d14
Dominic Midlane <dmidlane@spindogs.com>
2023-02-03 07:37:23 +0000

    operating system

---
+++ reference/os_demo.js
@@ -0,0 +1,26 @@
+// import { platform } from 'os';
+const os = require('os');
+
+// Platform
+console.log(os.platform());
+
+// CPU Architecture
+
+console.log(os.arch());
+
+// CPU Core info
+
+// console.log(os.cpus());
+
+// Free Memory
+console.log(os.freemem());
+
+// Total Memory
+console.log(os.totalmem());
+
+// Home Directory
+console.log(os.homedir());
+
+// Up Time in seconds
+
+console.log(os.uptime());
