410136c250ba994878dd8613151f921444c052d2
Dominic Midlane <dmidlane@spindogs.com>
2023-02-03 07:37:13 +0000

    file system

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -2,9 +2,10 @@ const fs = require('fs');
 const path = require('path');
 
 
-// Create folder
-// mkdir is async by default and take a callback fn for when the result is returned
-// there is a synchronous version mkdirSync()
+// // Create folder
+// // mkdir is async by default and take a callback fn for when the result is returned
+// // there is a synchronous version mkdirSync()
+
 fs.mkdir(path.join(__dirname,'/test'),{}, err => {
     if (err) throw err;
     console.log('folder created...');
@@ -12,16 +13,17 @@ fs.mkdir(path.join(__dirname,'/test'),{}, err => {
 
 // Create and write to a file
 // writeFile will overwrite the whole file content, appendFile will add
+
 fs.writeFile(
     path.join(__dirname,'/test','hello.txt'),
-    'File content here...',
+    'First File content...',
     err => {
         if (err) throw err;
         console.log('file created and written to...');
 
         fs.appendFile(
             path.join(__dirname,'/test','hello.txt'),
-            ' New content added from the callback fn...',
+            'New content added from the callback fn...',
             err => {
                 if (err) throw err;
                 console.log('file content added to...');
@@ -29,3 +31,21 @@ fs.writeFile(
         );
     }
 );
+
+// Read file - because it's async this would need to go in the callback of the other methods
+
+// fs.readFile(path.join(__dirname, '/test','hello.txt'),'utf8', (err,data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+// Rename file - because it's async this would need to go in the callback of the other methods
+
+// fs.rename(
+//     path.join(__dirname, '/test','hello.txt'),
+//     path.join(__dirname, '/test','hello-world.txt'),
+//     err => {
+//         if (err) throw err;
+//         console.log('file renamed');
+//     }
+// );
